bc7538a925b4c65900bf8cbdaeb4f37bfec3f0cf
Anna Sharova <asharova@wiley.com>
2019-04-11 19:06:38 +0300

    Initial commit

---
+++ src/main/java/ru/hackaton/calladocktor/CalladocktorApplication.java
@@ -0,0 +1,13 @@
+package ru.hackaton.calladocktor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalladocktorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalladocktorApplication.class, args);
+    }
+
+}
